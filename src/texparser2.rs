6569c83b21911a8abcaa2f802437dc47e9419d81
celvira <clement.elvira@inria.fr>
2019-04-03 23:58:16 +0200

    capturing macro and args (not nested one yet)

--- src/texparser2.rs
+++ src/texparser2.rs
@@ -61,7 +61,10 @@ pub mod texparser {
 	#[derive(Debug)]
 	enum TexParserState {
 		Empty,
+		InMacroName,
 		InMacro,
+		InMacroOptionalArg,
+		InMacroArg,
 	}
 
 	struct TexParser {
@@ -100,79 +103,127 @@ pub mod texparser {
 	    	match self.state {
 
 	    		TexParserState::Empty => {
+
 	    			if c == '\\' {
 	    				let texmacro = TexCmd::new(&String::from(""));
 	    				self.stack_macro.push(texmacro);
 
-	    				self.state = TexParserState::InMacro;
+	    				self.state = TexParserState::InMacroName;
 			    	}
 	    		}
 
-	    		TexParserState::InMacro => {
+	    		TexParserState::InMacroName => {
+
 	    			if c.is_alphabetic() {
 	    				// still in macro name
 	    				self.bufcmd.push(c);
 	    			}
 	    			else if c == '\\' {
 	    				// Start another macro
-	    				self.process_macro();	    				
+	    				self.set_macro_name_from_buf();
+	    				self.process_macro();
+
+	    				let texmacro = TexCmd::new(&String::from(""));
+	    				self.stack_macro.push(texmacro);    				
 	    			}
 					else if c == ' ' {
+						self.set_macro_name_from_buf();
 	    				self.process_macro();
-	    				self.state = TexParserState::Empty;
+
+	    				if self.stack_macro.len() == 0 {
+		    				self.state = TexParserState::Empty;
+		    			}
+					}
+
+					else if c == '[' {
+						self.set_macro_name_from_buf();
+						self.state = TexParserState::InMacroOptionalArg;
+					}
+
+					else if c == '{' {
+						self.set_macro_name_from_buf();
+						self.state = TexParserState::InMacroArg;
 					}
 
 					else {
+						self.set_macro_name_from_buf();
 						self.process_macro();
-	    				self.state = TexParserState::Empty;
-					}
 
-	    // 			else if c == '{' {
-	    // 				// Starting arguments
-	    // 			}
-					// else if c == '[' {
-					// 	// Starting optional argument
-					// }
+	    				if self.stack_macro.len() == 0 {
+		    				self.state = TexParserState::Empty;
+		    			}
+					}
 	    		}
 
+	    		TexParserState::InMacro => {
+	    			match c {
+	    				'{' => {
+	    					self.state = TexParserState::InMacroArg;
+	    				}
+
+	    				'[' => {
+	    					self.state = TexParserState::InMacroOptionalArg;
+	    				}
+
+	    				_ => {
+	    					self.process_macro();
+	    					if self.stack_macro.len() == 0 {
+		    					self.state = TexParserState::Empty;
+		    				}
+	    				}
+	    			}
+	    		}
+
+	    		TexParserState::InMacroOptionalArg => {
+	    			match c {
+	    				']' => {
+							let mut tex_macro = self.stack_macro.pop().unwrap();
+							tex_macro.add_opt_arg(&self.bufcmd);
+							self.stack_macro.push(tex_macro);
+
+							self.bufcmd = String::from("");
+							self.state = TexParserState::InMacro;
+	    				}
+
+	    				_ => {
+	    					self.bufcmd.push(c);
+	    				}
+	    			}
+	    		}
+
+				TexParserState::InMacroArg => {
+	    			match c {
+	    				'}' => {
+							let mut tex_macro = self.stack_macro.pop().unwrap();
+							tex_macro.add_arg(&self.bufcmd);
+							self.stack_macro.push(tex_macro);
+
+							self.bufcmd = String::from("");
+							self.state = TexParserState::InMacro;
+	    				}
+
+	    				_ => {
+	    					self.bufcmd.push(c);
+	    				}
+	    			}
+				}
 	    	}
 	    }
 
-		fn process_macro(&mut self) {
+	    fn set_macro_name_from_buf(&mut self) {
 			let mut tex_macro = self.stack_macro.pop().unwrap();
 			tex_macro.set_name(&self.bufcmd);
-			self.bufcmd = String::from(""); 
+			self.bufcmd = String::from("");
 
-			println!("{:?}", tex_macro.name);
-		}
-	}
-
-
-
-
-
-	fn process_main_loop(file: &File, _doc: &mut Document) {
-	    // -- Reader stuff
-	    let mut reader = BufReader::new(file);
-	    let mut buf = Vec::<u8>::new();
-
-	    // -- 
-	    //let 
-
-	    // -- starting reading loop
-	    while reader.read_until(b'\n', &mut buf).expect("read_until failed") != 0 {
-	        // this moves the ownership of the read data to s
-	        // there is no allocation
-	        let s = String::from_utf8(buf).expect("from_utf8 failed");
-	        for c in s.chars() {
-	            println!("Character: {}", c);
-	        }
-	        
-	        // this returns the ownership of the read data to buf
-	        // there is no allocation
-	        buf = s.into_bytes();
-	        buf.clear();
+			self.stack_macro.push(tex_macro);
 	    }
+
+		fn process_macro(&mut self) {
+			let tex_macro = self.stack_macro.pop().unwrap();
+
+			println!("name: {}, args: {:?}, opt: {:?}", 
+				tex_macro.name, tex_macro.args, tex_macro.option_args);
+		}
 	}
 
 	#[derive(Debug)]
@@ -193,6 +244,14 @@ pub mod texparser {
 
 		fn set_name(&mut self, cmd_name: &String) {
 			self.name = cmd_name.clone();
+		}
+
+		fn add_arg(&mut self, arg: &String) {
+			self.args.push(arg.clone());
+		}
+
+		fn add_opt_arg(&mut self, opt_arg: &String) {
+			self.option_args.push(opt_arg.clone());
 		}
 	}
 }
